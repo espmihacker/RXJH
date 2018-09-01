e5bb2232ad20f20415a1c5ad59317d1825f96317
Akasaka Ryuunosuke <mihacker.mh@gmail.com>
2018-09-02 00:49:16 +0800

    feat: 处理 *.sdf

--- VS2010Clear.bat
+++ VS2010Clear.bat
@@ -1,8 +1,8 @@
 REM vs2010 Clear
-del *.sdf *.log *.user *.ipch *.aps /s
+del *.sdf *.log *.user *.ipch *.aps *.orig /s
 del *.exe *.dll /s
 del *.suo /s /a h
 del *.ilk *.pdb *.exp *.lib *.tlog *.manifest *.res *.lastbuildstate /s
-del *.pch *.obj *.idb *.opensdf/s
+del *.pch *.obj *.idb *.opensdf /s
 
 pause
